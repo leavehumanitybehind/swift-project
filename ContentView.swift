bedcb43f91757bb08cc14e74b93fd9b441ba148b
stasy <bernastya@inbox.ru>
2021-11-12 11:19:57 +0300

    init

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  remotemode
+//
+//  Created by Anastasia on 12.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ remotemodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  remotemodeApp.swift
+//  remotemode
+//
+//  Created by Anastasia on 12.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct remotemodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
